9d9e39c6a56d4ca182bbf6de3932b2f5893b4fab
svanhoutte <22617546+svanhoutte@users.noreply.github.com>
2023-01-25 22:52:10 -0500

    Update to support # character
    Support # character in login and password

--- Docker files/entrypoint.sh
+++ Docker files/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-printenv > /etc/environment
-printenv
+touch /etc/cron.d/garmin_wyze_scheduler
+echo WYZE_EMAIL=$WYZE_EMAIL > /etc/cron.d/garmin_wyze_scheduler
+echo WYZE_PASSWORD=$WYZE_PASSWORD >> /etc/cron.d/garmin_wyze_scheduler
+echo WYZE_TOTP=$WYZE_TOTP >> /etc/cron.d/garmin_wyze_scheduler
+echo Garmin_password=$Garmin_password >> /etc/cron.d/garmin_wyze_scheduler
+echo Garmin_username=$Garmin_username >> /etc/cron.d/garmin_wyze_scheduler
+echo "*/10 * * * * { printf \"\%s: \" \"\$(date \"+\%F \%T\")\"; /wyze_garmin_sync/connect_sync.sh ; } >/proc/1/fd/1 2>/proc/1/fd/2"
+chmod 0644 /etc/cron.d/garmin_wyze_scheduler && crontab /etc/cron.d/garmin_wyze_scheduler
 /bin/bash ./connect_sync.sh
 cron -f
