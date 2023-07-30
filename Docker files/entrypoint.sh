b94e3c089d1d0dc384bd24f323f1886d3af541ef
svanhoutte <22617546+svanhoutte@users.noreply.github.com>
2023-07-29 21:32:57 -0400

    API key auth change

--- Docker files/entrypoint.sh
+++ Docker files/entrypoint.sh
@@ -3,7 +3,8 @@ touch /etc/cron.d/garmin_wyze_scheduler
 echo PATH=$PATH > /etc/cron.d/garmin_wyze_scheduler
 echo WYZE_EMAIL=$WYZE_EMAIL >> /etc/cron.d/garmin_wyze_scheduler
 echo WYZE_PASSWORD=$WYZE_PASSWORD >> /etc/cron.d/garmin_wyze_scheduler
-echo WYZE_TOTP=$WYZE_TOTP >> /etc/cron.d/garmin_wyze_scheduler
+echo WYZE_KEY_ID=$WYZE_KEY_ID >> /etc/cron.d/garmin_wyze_scheduler
+echo WYZE_API_KEY=$WYZE_API_KEY >> /etc/cron.d/garmin_wyze_scheduler
 echo Garmin_password=$Garmin_password >> /etc/cron.d/garmin_wyze_scheduler
 echo Garmin_username=$Garmin_username >> /etc/cron.d/garmin_wyze_scheduler
 echo "*/10 * * * * { printf \"\%s: \" \"\$(date \"+\%F \%T\")\"; /wyze_garmin_sync/connect_sync.sh ; } >/proc/1/fd/1 2>/proc/1/fd/2" >> /etc/cron.d/garmin_wyze_scheduler
