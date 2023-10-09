7add624f64f0e5a13955420b14464b9c9f852d78
svanhoutte <22617546+svanhoutte@users.noreply.github.com>
2023-10-09 17:43:45 -0400

    Update entrypoint.sh

--- Docker files/entrypoint.sh
+++ Docker files/entrypoint.sh
@@ -8,7 +8,7 @@ echo WYZE_KEY_ID=$WYZE_KEY_ID >> /etc/cron.d/garmin_wyze_scheduler
 echo WYZE_API_KEY=$WYZE_API_KEY >> /etc/cron.d/garmin_wyze_scheduler
 echo Garmin_password=$Garmin_password >> /etc/cron.d/garmin_wyze_scheduler
 echo Garmin_username=$Garmin_username >> /etc/cron.d/garmin_wyze_scheduler
-echo "*/10 * * * * { printf \"\%s: \" \"\$(date \"+\%F \%T\")\"; /wyze_garmin_sync/connect_sync.sh ; } >/proc/1/fd/1 2>/proc/1/fd/2" >> /etc/cron.d/garmin_wyze_scheduler
+echo "*/10 * * * * { printf \"\%s: \" \"\$(date \"+\%F \%T\")\"; /wyze_garmin_sync/scale.py ; } >/proc/1/fd/1 2>/proc/1/fd/2" >> /etc/cron.d/garmin_wyze_scheduler
 chmod 0644 /etc/cron.d/garmin_wyze_scheduler && crontab /etc/cron.d/garmin_wyze_scheduler
 chmod 0770 /wyze_garmin_sync/scale.py
 /bin/bash ./scale.py
