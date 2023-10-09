69662ff9a13747518c4e386806147f0c059c0b96
svanhoutte <22617546+svanhoutte@users.noreply.github.com>
2023-10-09 14:56:15 -0400

    Update entrypoint.sh

--- Docker files/entrypoint.sh
+++ Docker files/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir /etc/cron.d
 touch /etc/cron.d/garmin_wyze_scheduler
 echo PATH=$PATH > /etc/cron.d/garmin_wyze_scheduler
 echo WYZE_EMAIL=$WYZE_EMAIL >> /etc/cron.d/garmin_wyze_scheduler
@@ -10,4 +11,4 @@ echo Garmin_username=$Garmin_username >> /etc/cron.d/garmin_wyze_scheduler
 echo "*/10 * * * * { printf \"\%s: \" \"\$(date \"+\%F \%T\")\"; /wyze_garmin_sync/connect_sync.sh ; } >/proc/1/fd/1 2>/proc/1/fd/2" >> /etc/cron.d/garmin_wyze_scheduler
 chmod 0644 /etc/cron.d/garmin_wyze_scheduler && crontab /etc/cron.d/garmin_wyze_scheduler
 /bin/bash ./connect_sync.sh
-cron -f
+crond -f
